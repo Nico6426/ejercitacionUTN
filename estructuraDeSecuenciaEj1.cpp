7c6428080cec7119ab7f61c681292c72c4603f00
Eduardo <69368644+EduardoChavero@users.noreply.github.com>
2020-09-11 00:04:20 -0300

    Add files via upload

---
+++ estructuraDeSecuenciaEj1.cpp
@@ -0,0 +1,21 @@
+/*
+Hacer un programa para ingresar por teclado la cantidad de
+horas trabajadas por un operario y el valor que se le paga
+por hora trabajada y listar por pantalla el sueldo que le corresponda.
+*/
+
+
+
+#include <iostream>
+using namespace std;
+
+int main (){
+float horas, valorHora;
+cout << "Ingrese la cantidad de horas trabajadas: " << endl;
+cin >> horas;
+cout << "Ingrese valor de la hora de trabajo: " << endl;
+cin >> valorHora;
+cout << "Importe a cobrar: $" << horas*valorHora;
+
+return 0;
+}
